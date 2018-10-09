248dfb14d9e8630ed7b103a4ec8b192cdd68a7ba
niuyunna <18310413634@163.com>
2018-10-09 18:20:47 +0800

    新增后台项目

---
+++ src/api/api-type.js
@@ -0,0 +1,63 @@
+/* ************** 所有api的配置文档 ***************/
+export const BASE_URL = 'http://mjwhqt.hjw988.com'// base url
+
+/* ************** 后台接口 ***************/
+
+export const GROUP_ADD = BASE_URL + '/admin/group/add' // 添加管理组
+export const GROUP_UPDATE = BASE_URL + '/admin/group/update' // 修改管理组
+export const GROUP_LIST = BASE_URL + '/admin/group/list' // 管理组列表
+export const GROUP_DETAIL = BASE_URL + '/admin/group/detail' // 管理组详情
+export const GROUP_DELETE = BASE_URL + '/admin/group/delete' // 删除管理组
+export const ROULE_UPDATE = BASE_URL + '/admin/rule/update' // 设置权限
+export const ROULE_DETAIL = BASE_URL + '/admin/rule/detail' // 获取权限
+/* **** 资讯 **** */
+export const INFO_ADD = BASE_URL + '/admin/info/add' // 资讯添加
+export const INFO_UPDATE = BASE_URL + '/admin/info/update' // 资讯修改
+export const INFO_LIST = BASE_URL + '/admin/info/list' // 资讯列表
+export const INFO_DETAIL = BASE_URL + '/admin/info/detail' // 资讯详情
+export const INFO_DELETE = BASE_URL + '/admin/info/delete' // 资讯删除
+/* **** 商品 **** */
+export const GOODS_ADD = BASE_URL + '/admin/goods/add' // 商品添加
+export const GOODS_UPDATE = BASE_URL + '/admin/goods/update' // 商品修改
+export const GOODS_LIST = BASE_URL + '/admin/goods/list' // 商品列表
+export const GOODS_DETAIL = BASE_URL + '/admin/goods/detail' // 商品详情
+export const GOODS_STATUS = BASE_URL + '/admin/goods/status' // 商品上架/下架
+export const GOODS_TYPE = BASE_URL + '/admin/goods/type' // 商品推荐/不推荐的接口
+export const GOODS_DELETE = BASE_URL + '/admin/goods/delete' // 商品删除
+/* **** 留言管理 **** */
+export const CANTACT_UPDATE = BASE_URL + '/admin/contact/update' // 留言管理处理
+export const CANTACT_LIST = BASE_URL + '/admin/contact/list' // 留言管理列表
+/* **** 分类 **** */
+export const CATEGORY_ADD = BASE_URL + '/admin/category/add' // 分类添加
+export const CATEGORY_UPDATE = BASE_URL + '/admin/category/update' // 分类修改
+export const CATEGORY_LIST = BASE_URL + '/admin/category/list' // 分类列表
+export const CATEGORY_DETAIL = BASE_URL + '/admin/category/detail' // 分类详情
+export const CATEGORY_DELETE = BASE_URL + '/admin/category/delete' // 分类删除
+/* **** 用户 **** */
+export const USER_LIST = BASE_URL + '/admin/user/list' // 用户列表
+/* **** 订单 **** */
+export const ORDER_UPDATE = BASE_URL + '/admin/order/update' // 订单发货
+export const ORDER_LIST = BASE_URL + '/admin/order/list' // 订单列表
+export const ORDER_SN = BASE_URL + '/admin/order/sn' // 订单号搜索
+export const ORDER_SEARCH = BASE_URL + '/admin/order/search' // 订单检索
+export const ORDER_DETAIL = BASE_URL + '/admin/order/detail' // 订单详情
+/* **** 管理员 **** */
+export const ADMIN_ADD = BASE_URL + '/admin/add' // 添加管理员
+export const ADMIN_LOGIN = BASE_URL + '/admin/login' // 登录
+export const ADMIN_LOGOUT = BASE_URL + '/admin/logout' // 登出
+export const ADMIN_UPDATE = BASE_URL + '/admin/update' // 修改管理员
+export const ADMIN_LIST = BASE_URL + '/admin/list' // 管理员列表
+export const ADMIN_DETAIL = BASE_URL + '/admin/detail' // 管理员详情
+export const ADMIN_FREEZE = BASE_URL + '/admin/freeze' // 冻结管理员
+export const ADMIN_UNFREEZE = BASE_URL + '/admin/unfreeze' // 解冻管理员
+export const ADMIN_DELETE = BASE_URL + '/admin/delete' // 删除管理员
+/* **** banner **** */
+export const BANNER_ADD = BASE_URL + '/admin/banner/add' // banner添加
+export const BANNER_UPDATE = BASE_URL + '/admin/banner/update' // banner修改
+export const BANNER_LIST = BASE_URL + '/admin/banner/list' // banner列表
+export const BANNER_DETAIL = BASE_URL + '/admin/banner/detail' // banner详情
+export const BANNER_DELETE = BASE_URL + '/admin/banner/delete' // banner删除
+
+/* ******************** 通用接口 ******************* */
+export const API_UPLOADS = BASE_URL + '/api/uploads' // 多图上传
+export const API_UPLOAD = BASE_URL + '/api/upload' // 单图上传
---
+++ src/api/index.js
@@ -0,0 +1,114 @@
+/* ********************* request 全局配置文档 ************************/
+import Vue from 'vue'
+import axios from 'axios'
+// import router from '../router'
+import qs from 'qs'
+import { Toast } from 'vant'
+
+// 请求错误处理 注意根据具体的服务器端设置，此处仅仅是个示例
+const errorFn = err => {
+  console.log(err.toString())
+  if (err.toString().indexOf('timeout') !== -1) {
+    Toast('请求超时，请稍后再试')
+  }
+  if (err && err.response) {
+    switch (err.response.status) {
+      case 400:
+        err.message = '请求错误'
+        break
+
+      case 401:
+        err.message = '未授权，请登录'
+        break
+
+      case 403:
+        err.message = '拒绝访问'
+        break
+
+      case 404:
+        err.message = `请求地址出错: ${err.response.config.url}`
+        break
+
+      case 408:
+        err.message = '请求超时'
+        break
+
+      case 500:
+        err.message = '服务器开个小差，请稍后再试'
+        break
+
+      case 501:
+        err.message = '服务器开个小差，请稍后再试'
+        break
+
+      case 502:
+        err.message = '服务器开个小差，请稍后再试'
+        break
+
+      case 503:
+        err.message = '服务器开个小差，请稍后再试'
+        break
+
+      case 504:
+        err.message = '服务器开个小差，请稍后再试'
+        break
+
+      case 505:
+        err.message = 'HTTP版本不受支持'
+        break
+
+      default:
+        err.message = '请求失败，请稍后再试'
+    }
+    Vue.prototype.$message.error(err.message)
+  }
+  return Promise.reject(err)
+}
+/* ***此处开始封装不同后台下的axios实例 ****/
+
+const $axios = axios.create({
+  headers: {
+  },
+  // baseURL: BASE_URL,
+  timeout: 30000
+})
+$axios.interceptors.request.use(
+  config => {
+    if (config.method === 'post') {
+      config.data = qs.stringify(config.data)
+    }
+    if (process.env.NODE_ENV === 'development') {
+      // 这里一个token，你需要在这里取到你设置好的token的值
+      // const token = localStorage['X-LONGCHAT-Token']
+      // const token = '5cc915c437c43fb8129c2437db72d734'
+      // if (token) {
+      //   // 这里将token设置到headers中，header的key是Authorization，这个key值根据你的需要进行修改即可
+      //   config.headers = {
+      // 'token': token
+      // 'User-Agent': navigator.userAgent
+      // }
+      // }
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+$axios.interceptors.response.use(
+  response => {
+    // 如果状态码是ES7008，表示token失效，则直接跳转到登录页面
+    if (response.data.error_code === 5002) {
+      Toast(response.data.error_msg)
+      // setTimeout(function () {
+      //   router.replace({
+      //     path: '/login'
+      //   })
+      // }, 1500)
+      // window.location.href = 'http://mjwhqt.hjw988.com/api/login'
+    } else {
+      return response
+    }
+  }, errorFn
+)
+Vue.prototype.$axios = $axios
