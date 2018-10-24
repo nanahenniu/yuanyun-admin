173fb801cbda85c20e602faaf6d6685adc1ad2b3
zhangjiayu <498701604@qq.com>
2018-10-24 16:26:05 +0800

    admin category

--- src/components/page/message/Message.vue
+++ src/components/page/message/Message.vue
@@ -1,12 +1,6 @@
 <template>
     <div>
         <div class="container">
-            <div class="handle-box">
-                <el-row :gutter="20">
-                    <el-col :span="4" class="total-num">共有数据： {{totalPage}}条</el-col>
-                    <el-col :span="20" align="right"><el-button type="primary" icon="search" @click="createInfo">新建</el-button></el-col>
-                </el-row>
-            </div>
             <el-table :data="tableData" border class="table" ref="multipleTable">
                 <el-table-column prop="id" label="ID" align="center">
                     <!--<template slot-scope="scope">-->
@@ -24,8 +18,8 @@
                 </el-table-column>
                 <el-table-column label="处理状态" align="center" width="200">
                     <template slot-scope="scope">
-                        <el-button type="text" icon="el-icon-view" class="red" @click="preview(scope.$index, scope.row)">查看</el-button>
-                        <el-button type="text" icon="el-icon-delete" class="red" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+                        <el-button v-if="scope.row.status===0" type="primary" @click="toDone(scope.$index, scope.row)">未处理</el-button>
+                        <el-button v-else type="info" disabled>已处理</el-button>
                     </template>
                 </el-table-column>
             </el-table>
@@ -36,10 +30,10 @@
         </div>
         <!-- 删除提示框 -->
         <el-dialog title="提示" :visible.sync="delVisible" width="300px" center>
-            <div class="del-dialog-cnt">删除不可恢复，是否确定删除？</div>
+            <div class="del-dialog-cnt">是否确认操作？</div>
             <span slot="footer" class="dialog-footer">
             <el-button @click="delVisible = false">取 消</el-button>
-            <el-button type="primary" @click="deleteRow">确 定</el-button>
+            <el-button type="primary" @click="toDoneConfirm">确 定</el-button>
         </span>
         </el-dialog>
     </div>
@@ -47,7 +41,7 @@
 
 <script>
     import { CANTACT_UPDATE, CANTACT_LIST } from '@/api/api-type'
-    
+
     export default {
         name: 'Message',
         data() {
@@ -57,8 +51,7 @@
                 cur_page: 1,
                 totalPage: 0, // 总页数
                 delVisible: false,
-                idx: -1,
-                adminId: 0,
+                contactId: 0,
             }
         },
         created() {
@@ -71,39 +64,36 @@
                 this.getData();
             },
             getData() {
+                let _this = this
                 this.$axios.post(CANTACT_LIST, {
-                    page: this.cur_page,
+                    page: _this.cur_page,
                     pagesize: 10,
-                    token: this.token
+                    token: _this.token
                 }).then((res) => {
                     if (res.data.error_code === 0) {
                         console.log(res.data.data.data)
-                        this.totalPage = res.data.data.count
-                        this.tableData = res.data.data.data;
+                        _this.totalPage = res.data.data.count
+                        _this.tableData = res.data.data.data;
                     }
                     if (res.data.error_code === 1010) {
-                        this.totalPage = 0
+                        _this.totalPage = 0
                     }
                 })
             },
-            // 新建
-            createInfo() {
-                this.$router.push('/addadmin')
-            },
-            handleDelete(index, row) {
-                this.idx = index;
-                this.adminId = row.id;
+            toDone(index, row) {
+                this.contactId = row.id;
                 this.delVisible = true;
             },
-            // 确定删除
-            deleteRow(){
-                this.tableData.splice(this.idx, 1);
-                this.$axios.post(ADMIN_DELETE, {token: this.token, admin_id: this.adminId}).then(res => {
+            // 确定处理
+            toDoneConfirm(){
+                let _this = this
+                this.$axios.post(CANTACT_UPDATE, {token: _this.token, contact_id: _this.contactId}).then(res => {
                     if (res.data.error_code == 0) {
-                        this.$message.success('删除成功');
-                        this.delVisible = false;
+                        _this.$message.success('删除成功');
+                        _this.delVisible = false;
+                        _this.getData()
                     } else {
-                        this.$message.waiting(res.data.error_msg)
+                        _this.$message.waiting(res.data.error_msg)
                     }
                 })
             },
